ab32b1cf833dbcff89685956ca0939358d1c4aea
rabbyhasan <rabbyhasan4594@gmail.com>
2023-03-28 12:47:27 +0600

    cart added compiled

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -3,23 +3,30 @@ import Product from '../product/Product';
 import './Shop.css'
 const Shop = () => {
     const [products, setProducts] = useState([]);
+    const [cart,setCart]=useState([])
     useEffect(() => {
         fetch('products.json')
             .then(res => res.json())
             .then(data => setProducts(data))
     })
+    const handleAddToCart =(product) =>{
+        const newCart=[...cart,product];
+        setCart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="products-container">
                 {
                     products.map(product => <Product
                         key={product.id}
-                        product={product} >
+                        product={product} 
+                        handleAddToCart={handleAddToCart} >
                     </Product>)
                 }
             </div>
             <div className="cart-container">
                 <h4>Oder Summary</h4>
+                <p>Selected:{cart.length}</p>
             </div>
         </div>
     );
--- src/components/product/Product.jsx
+++ src/components/product/Product.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import './Product.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
 const Product = (props) => {
-    const{img,name,seller,quantity,price,ratings}=props.product;
+    const{img,name,seller,price,ratings}=props.product;
+    const handleAddToCart=props.handleAddToCart;
+    
     return (
         <div className="product">
             <img src={img} alt="" />
@@ -11,7 +15,8 @@ const Product = (props) => {
             <p>Manufacturer :{seller}</p>
             <p>Ratings: {ratings} Stars</p>
             </div>
-            <button className='btn-cart'>Add to Cart</button>
+            <button onClick={ () =>handleAddToCart(props.product)} className='btn-cart'>Add to Cart
+            <FontAwesomeIcon icon={faShoppingCart} /></button>
         </div>
     );
 };
